458f3ef55667f327248ba9f3e7b6bef629df2292
kbaduwal <khakendra1999@gmail.com>
2025-01-08 16:10:31 +0545

    First commit

---
+++ ss_2025_l1_ex1/src/main/java/com/example/ss_2025_l1_ex1/Ss2025L1Ex1Application.java
@@ -0,0 +1,13 @@
+package com.example.ss_2025_l1_ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ss2025L1Ex1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ss2025L1Ex1Application.class, args);
+    }
+
+}
---
+++ ss_2025_l1_ex1/src/test/java/com/example/ss_2025_l1_ex1/Ss2025L1Ex1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ss_2025_l1_ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ss2025L1Ex1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
