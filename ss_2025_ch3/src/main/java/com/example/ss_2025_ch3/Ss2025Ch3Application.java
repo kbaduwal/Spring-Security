69fbbef304aebc147683296d71e5986a63d1c973
kbaduwal <khakendra1999@gmail.com>
2025-01-09 16:02:24 +0545

    Initial Commit

---
+++ ss_2025_ch3/src/main/java/com/example/ss_2025_ch3/Ss2025Ch3Application.java
@@ -0,0 +1,13 @@
+package com.example.ss_2025_ch3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ss2025Ch3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ss2025Ch3Application.class, args);
+	}
+
+}
