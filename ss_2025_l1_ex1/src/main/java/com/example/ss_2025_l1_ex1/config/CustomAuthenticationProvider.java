63d5b55d3093ad01cd9ae69c6124098c5bf48eb6
kbaduwal <khakendra1999@gmail.com>
2025-01-09 14:31:51 +0545

    implementation of the authentication provider

---
+++ ss_2025_l1_ex1/src/main/java/com/example/ss_2025_l1_ex1/config/CustomAuthenticationProvider.java
@@ -0,0 +1,45 @@
+package com.example.ss_2025_l1_ex1.config;
+
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+/*The AuthenticationProvider implements the authentication logic.
+It receives the request from the AuthenticationManager and
+delegates finding the user to a UserDetailsService, verifying the password to a PasswordEncoder.*/
+@Component
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+    @Override
+    public Authentication authenticate(Authentication authentication)
+            throws AuthenticationException {
+        // authentication logic here
+        String username =authentication.getName();
+        String password = String.valueOf(authentication.getCredentials());
+
+        if("john".equals(username) && "12345".equals(password)) {
+            return new UsernamePasswordAuthenticationToken(
+                    username,
+                    password,
+                    Arrays.asList());
+        }else {
+            throw new
+                    AuthenticationCredentialsNotFoundException("Error");
+        }
+
+    }
+
+    @Override
+    public boolean supports(Class<?> authenticationTye) {
+        // type of the Authentication implementation here
+
+        return UsernamePasswordAuthenticationToken
+                .class
+                .isAssignableFrom(authenticationTye);
+    }
+}
--- ss_2025_l1_ex1/src/main/java/com/example/ss_2025_l1_ex1/config/ProjectConfig.java
+++ ss_2025_l1_ex1/src/main/java/com/example/ss_2025_l1_ex1/config/ProjectConfig.java
@@ -2,6 +2,7 @@ package com.example.ss_2025_l1_ex1.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -12,7 +13,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectConfig {
-
+/*
     @Bean
     UserDetailsService userDetailsService(){
         var user = User.withUsername("Lucas")
@@ -22,14 +23,39 @@ public class ProjectConfig {
         return new InMemoryUserDetailsManager(user);
     }
 
+
     @Bean
     PasswordEncoder passwordEncoder(){
         return NoOpPasswordEncoder.getInstance();
     }
 
+ */
+
     //Defining a SecurityFilterChain bean
     @Bean
     SecurityFilterChain configure(HttpSecurity http) throws Exception {
+
+        http.httpBasic(Customizer.withDefaults());
+
+        http.authorizeHttpRequests(
+                c->c.anyRequest().authenticated()
+        );
+
+        //Setting UserDetailsService with the SecurityFilterChain bean
+        var user = User.withUsername("john")
+                .password("12345")
+                .roles("read")
+                .build();
+
+        var userDetailsService = new InMemoryUserDetailsManager(user);
+
+        http.userDetailsService(userDetailsService);
+
         return http.build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
