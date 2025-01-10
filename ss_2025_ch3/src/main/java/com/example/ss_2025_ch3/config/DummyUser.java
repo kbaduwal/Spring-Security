fb51014c0bf99c997acbda75fe13bdbb50602cbd
kbaduwal <khakendra1999@gmail.com>
2025-01-10 13:18:27 +0545

    Implementation of the getAuthorities() method

--- ss_2025_ch3/src/main/java/com/example/ss_2025_ch3/config/DummyUser.java
+++ ss_2025_ch3/src/main/java/com/example/ss_2025_ch3/config/DummyUser.java
@@ -19,7 +19,7 @@ public class DummyUser implements UserDetails{
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return List.of(()->"READ");
     }
 
     @Override
