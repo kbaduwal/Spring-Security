c062d71153cb8dd22544ba8b49eb6123300730c4
kbaduwal <khakendra1999@gmail.com>
2025-01-10 13:16:57 +0545

    implementing the methods for the UserDetails contract

---
+++ ss_2025_ch3/src/main/java/com/example/ss_2025_ch3/config/DummyUser.java
@@ -0,0 +1,44 @@
+package com.example.ss_2025_ch3.config;
+//first implementation of the UserDetails contract
+
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+import java.util.List;
+
+public class DummyUser implements UserDetails{
+    @Override
+    public String getUsername() {
+        return "bill";
+    }
+
+    @Override
+    public String getPassword() {
+        return "12345";
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
